d3c93d5ff5af989919f77824a5320f7ab2244869
zebn <zebn@bk.ru>
2022-11-25 22:42:21 +0100

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { ElementSchemaRegistry } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styles: [`
+  .online {
+  color: white;
+  }
+  `],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'tarea3';
+  serverCreated = true;
+  servers = [1];
+
+  mostrarDetalles() {
+    this.serverCreated = (this.serverCreated) ? false : true;
+    this.servers.push(this.servers.length + 1);
+  }
+
+  getColor() {
+    if (this.servers.length > 5)
+      return 'blue'
+      else
+      return 'white'
+  }
+
 }
